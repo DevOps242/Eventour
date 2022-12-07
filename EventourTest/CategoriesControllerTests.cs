1983a3ce96cc68572075699d8c799100eb9b2a4f
Akeem Palmer <akeemk.palmer@gmail.com>
2022-12-07 16:07:55 -0500

    Update: added test for Edit Methods

--- EventourTest/CategoriesControllerTests.cs
+++ EventourTest/CategoriesControllerTests.cs
@@ -174,10 +174,11 @@ public class CategoriesControllerTests
         categoryController.ModelState.AddModelError("Name", "Name can't be empty");
 
         // Act
-        var result = categoryController.Create(model).Result;
+        var result = (ViewResult)categoryController.Create(model).Result;
 
         // Assert
-        Assert.IsTrue(categoryController.ModelState["Name"].Errors.Count > 0);
+        //Assert.IsTrue(categoryController.ModelState["Name"].Errors.Count > 0);
+        Assert.AreEqual(categoryController.ModelState["Name"].Errors.ToString, result.ToString);
     }
 
 
@@ -195,10 +196,205 @@ public class CategoriesControllerTests
         //Assert.IsTrue(categoryController.ModelState["Description"].Errors.Count > 0);
         Assert.AreEqual(categoryController.ModelState["Description"].Errors.ToString, result.ExecuteResultAsync);
     }
-
-    
+    #endregion
 
     //    //Use controller.ModelState.AddModelError("put a descriptive key name here", "add an appropriate key value here");
     //    //to test any parts of methods that handle errors in the model validation(the CREATE and EDIT POST methods).
+
+    #region "Edit Tests"
+
+    [TestMethod]
+    public void GetEditNoIdLoads404()
+    {
+        // Act
+        var result = (ViewResult)categoryController.Edit(null).Result;
+
+        // Assert
+        Assert.AreEqual("404", result.ViewName);
+    }
+
+    [TestMethod]
+    public void GetEditNoCategoryTableLoads404()
+    {
+        context.Categories = null;
+        // Act
+        var result = (ViewResult)categoryController.Edit(null).Result;
+
+        // Assert
+        Assert.AreEqual("404", result.ViewName);
+    }
+
+    [TestMethod]
+    public void GetEditInvalidIdLoads404()
+    {
+        // Act
+        var result = (ViewResult)categoryController.Edit(500).Result;
+
+        // Assert
+        Assert.AreEqual("404", result.ViewName);
+    }
+
+    [TestMethod]
+    public void GetEditIsValidLoadsView()
+    {
+        // Act
+        var result = (ViewResult)categoryController.Edit(1001).Result;
+
+        // Assert
+        Assert.AreEqual("Edit", result.ViewName);
+    }
+
+    [TestMethod]
+    public void GetEditIsValidLoadsCategory()
+    {
+        // Act
+        var result = (ViewResult)categoryController.Edit(1001).Result;
+
+        // Assert
+        Assert.AreEqual(context.Categories.Find(1001), result.Model);
+    }
+
+    [TestMethod]
+    public void PostEditInvalidIDReturn404()
+    {
+        var model = context.Categories.Find(1003);
+
+        // Act
+        var result = (ViewResult)categoryController.Edit(1004, model).Result;
+
+        // Assert
+        Assert.AreEqual("404", result.ViewName);
+    }
+
+    [TestMethod]
+    public void PostEditNotFoundIDReturn404()
+    {
+        // Act
+        var result = (ViewResult)categoryController.Edit(100000).Result;
+
+        // Assert
+        Assert.AreEqual("404", result.ViewName);
+    }
+
+    [TestMethod]
+    public void PostEditCategoryValidReturnsIndex()
+    {
+        var model = context.Categories.Find(1003);
+
+        model.Name = "Editing Name";
+        model.Description = "Editing Description";
+        model.Color = "Editing Color";
+
+        // Act
+        var result = (RedirectToActionResult)categoryController.Edit(1003, model).Result;
+
+        // Assert
+        Assert.AreEqual("Index", result.ActionName);
+    }
+
+    [TestMethod]
+    public void PostEditCategoryInvalidReturnsEditView()
+    {
+        var model = context.Categories.Find(1003);
+
+        model.Name = null;
+        model.Description = "Editing Description";
+        model.Color = "Editing Color";
+
+        categoryController.ModelState.AddModelError("Form", "Invalid Form");
+
+        // Act
+        var result = (ViewResult)categoryController.Edit(1003, model).Result;
+
+        // Assert
+        Assert.AreEqual("Edit", result.ViewName);
+    }
+
+    //// Come back here.
+
+    [TestMethod]
+    public void PostEditCategoryNullNameReturnsError()
+    {
+        // Arrange
+        var model = context.Categories.Find(1004);
+        model.Name = null;
+        categoryController.ModelState.AddModelError("Name", "Name can't be empty");
+
+        // Act
+        var result = (ViewResult)categoryController.Edit(1004, model).Result;
+
+        // Assert
+        //Assert.IsTrue(categoryController.ModelState["Name"].Errors.Count > 0);
+        Assert.AreEqual(categoryController.ModelState["Name"].Errors.ToString, result.ToString);
+    }
+
+
+    [TestMethod]
+    public void PostEditCategoryNullDescriptionReturnsError()
+    {
+        // Arrange
+        var model = context.Categories.Find(1004);
+        model.Description = null;
+        categoryController.ModelState.AddModelError("Description", "Description can't be empty");
+
+        // Act
+        var result = (ViewResult)categoryController.Edit(1004, model).Result;
+
+        // Assert
+        //Assert.IsTrue(categoryController.ModelState["Description"].Errors.Count > 0);
+        Assert.AreEqual(categoryController.ModelState["Description"].Errors.ToString, "Description can't be empty");
+    }
     #endregion
+
+
+    #region "Delete Tests"
+
+    //    // GET: Categories/Delete/5
+    //    public async Task<IActionResult> Delete(int? id)
+    //    {
+    //        if (id == null || _context.Categories == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        var category = await _context.Categories
+    //            .FirstOrDefaultAsync(m => m.CategoryId == id);
+    //        if (category == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        return View(category);
+    //    }
+
+    //    // POST: Categories/Delete/5
+    //    [HttpPost, ActionName("Delete")]
+    //    [ValidateAntiForgeryToken]
+    //    public async Task<IActionResult> DeleteConfirmed(int id)
+    //    {
+    //        if (_context.Categories == null)
+    //        {
+    //            return Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
+    //        }
+    //        var category = await _context.Categories.FindAsync(id);
+    //        if (category != null)
+    //        {
+    //            _context.Categories.Remove(category);
+    //        }
+
+    //        await _context.SaveChangesAsync();
+    //        return RedirectToAction(nameof(Index));
+    //    }
+
+    //    private bool CategoryExists(int id)
+    //    {
+    //        return (_context.Categories?.Any(e => e.CategoryId == id)).GetValueOrDefault();
+    //    }
+    //}
+    #endregion
+
+
+
+
+
 }
